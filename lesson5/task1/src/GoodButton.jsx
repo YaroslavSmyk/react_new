c82dec5873cf26840932922c3628dcbe04a5c31f
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-05-25 11:17:00 +0300

    v1

---
+++ lesson5/task1/src/GoodButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GoodButton = () => {
+  return (
+    <button className="fance-button" onClick={() => alert('Good job!')}>
+      Click me!
+    </button>
+  );
+};
+
+export default GoodButton;

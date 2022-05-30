bed48bece8f7847a996eec164c011b1075c90089
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-05-30 16:53:26 +0300

    v1

---
+++ lesson6/task4/src/Online.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Online = () => {
+  return (
+    <div className="status__text">All good</div>
+  )
+}
+  
+export default Online;
---
+++ lesson6/task4/src/Status.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Online from './Online';
+import Offline from './Offline';
+
+// const online = 'All good';
+// const offline = 'Offline';
+
+class Status extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOnline: false,
+    };
+  }
+
+  setText = () => {
+    this.setState({
+      isOnline: true,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+          {this.state.isOnline? <Online isOnline={this.state.isOnline}/> :
+          <Offline isOnline={this.setText}/>}
+      </div>
+    );
+  }
+}
+
+export default Status;

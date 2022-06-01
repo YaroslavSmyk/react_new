de6bbb0c639ae88dc3b309b825d6e672da6ed031
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-06-01 16:16:37 +0300

    v3

--- lesson8/task1/src/Index.jsx
+++ lesson8/task1/src/Index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.scss';
-import Life from './Life';
+import Demo from './Demo.jsx';
 
 const rootElement = document.querySelector("#root");
 
 
 
-ReactDOM.render(<Life />, rootElement);
+ReactDOM.render(<Demo />, rootElement);

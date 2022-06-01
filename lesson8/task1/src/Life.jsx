e3a49157eb2e6a05648cccf59a2bdad064c9aae3
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-06-01 16:27:19 +0300

    v9

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -4,7 +4,6 @@ class Life extends Component {
   constructor(props) {
     super(props);
     console.log('constructor: good place to create state');
-    return nextProps.number % 2;
   }
 
   componentDidMount() {
@@ -13,23 +12,20 @@ class Life extends Component {
 
   componentWillUnmount() {
     console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
-
   }
+
   shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-}
-
-componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
     return true;
-}
+  }
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+  }
 
   render() {
-      console.log('return React element to build DOM');
-   return <div className='number'>
-{this.props.number}
-   </div>
+    console.log('return React element to build DOM');
+    return <div>{this.props.number}</div>;
   }
 }
 

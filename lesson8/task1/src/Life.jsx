4452ff8da8d8ed4aea62efc887d0ab618c3b5ec6
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-06-01 16:03:22 +0300

    v1

---
+++ lesson8/task1/src/Life.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Life extends Component {
+  constructor(props) {
+    super(props);
+    console.log('consrtuctor: good place to creat state');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount: API calls, subscriptions');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount(): cleanup befor DOM related to component will be removed');
+
+  }
+
+  render() {
+      console.log('return React element to build DOM');
+   return <div className='number'>
+{this.props.number}
+   </div>
+  }
+}
+
+export default Life;

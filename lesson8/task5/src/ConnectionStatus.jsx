916b72ed28c6e0137c8086c2566166f8e06a1ba5
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-06-02 14:47:09 +0300

    v1

--- lesson8/task5/src/ConnectionStatus.jsx
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
 
 class ConnectionStatus extends Component {
- 
+  state = {
+    statusOn: true,
+  };
+
+  componentDidMount() {
+    window.addEventListener('online', this.handeleOn);
+    window.addEventListener('offline', this.handeleOff);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.handeleOn);
+    window.removeEventListener('offline', this.handeleOff);
+  }
+
+  handeleOff = () => {
+    this.setState({
+      statusOn: false,
+    });
+  };
+  handeleOn = () => {
+    this.setState({
+      statusOn: true,
+    });
+  };
+
+  render() {
+    // const { statusOn } = this.state;
+
+    return this.state.statusOn ? (
+      <div className="status">online</div>
+    ) : (
+      <div className="status status_offline">offline</div>
+    );
+  }
 }
 
 export default ConnectionStatus;

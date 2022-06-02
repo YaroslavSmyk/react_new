f2f1bfe2f64c776eae983fa5e52ff6171d9d5bdd
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-06-02 18:26:07 +0300

    v3

---
+++ lesson9/task2/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import UserForm from './UserForm';
+
+class App extends Component {
+  createUser = (userData) => {
+    console.log(userData);
+  };
+
+  render() {
+    return <UserForm onSubmit={this.createUser} />;
+  }
+}
+// const App = () => {
+
+// }
+
+export default App;

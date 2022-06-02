dc5236908ba11bb1512ee372bea59ad65c7017e9
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-06-02 18:45:21 +0300

    v4

--- lesson9/task2/src/App.jsx
+++ lesson9/task2/src/App.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 import UserForm from './UserForm';
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+      }
   createUser = (userData) => {
-    console.log(userData);
+    // console.log(userData);
   };
 
   render() {
     return <UserForm onSubmit={() => this.createUser} />;
   }
 }
-// const App = () => {
-
-// }
 
 export default App;
--- lesson9/task2/src/UserForm.jsx
+++ lesson9/task2/src/UserForm.jsx
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
 
-
 class UserForm extends Component {
-  state = { 
-    name: '',
-    student: '',
-    occupation: '',
-    about: '',
- };
+  constructor(props) {
+    super(props)
+    // console.log(props)
+    this.state = {
+      name: '',
+      student: '',
+      occupation: '',
+      about: '',
+    };
+  }
 
-handleChange = event => {
-  const { name, value, checked, type } = event.target;
+  handleChange = (event) => {
+    const { name, value, checked, type } = event.target;
 
-  const val = type === 'checkbox'
-  ? checked:
-  value;
+    const val = type === 'checkbox' ? checked : value;
 
-  this.setState({
-    [name]: val,
-  }) ;
-}
+    this.setState({
+      [name]: val,
+    });
+  };
 
-handleSubmit = event => {
-  event.preventDefault();
-  console.log(this.state)
-}
+  handleSubmit = (event) => {
+    // console.log(55555555555555)
+    event.preventDefault();
+    // console.log(this.state)
+
+    this.props.onSubmit(this.state)
+    // console.log(6666666)
+
+    // console.log(this.state);
+  };
 
   render() {
+    // console.log(this.state)
+    // console.log(this.props)
     return (
       <form className="login-form" onSubmit={this.handleSubmit}>
         <h1 className="form-title">Profile</h1>
@@ -35,7 +44,8 @@ handleSubmit = event => {
             Name
           </label>
           <input
-            className="form-input" value={this.state.name}
+            className="form-input"
+            value={this.state.name}
             type="text"
             id="name"
             name="name"
@@ -47,7 +57,8 @@ handleSubmit = event => {
             Student
           </label>
           <input
-            className="form-input" value={this.state.student}
+            className="form-input"
+            value={this.state.student}
             type="checkbox"
             id="student"
             name="student"
@@ -55,10 +66,15 @@ handleSubmit = event => {
           />
         </div>
         <div className="form-control">
-          <label className="form-label"  id="occupation" htmlFor="occupation">
+          <label className="form-label" id="occupation" htmlFor="occupation">
             Occupation
           </label>
-          <select name="occupation" value={this.state.occupation} onChange={this.handleChange} className="form-input" >
+          <select
+            name="occupation"
+            value={this.state.occupation}
+            onChange={this.handleChange}
+            className="form-input"
+          >
             <option value="london">London</option>
             <option value="new-york">New York</option>
             <option value="coconut">Sidney</option>
@@ -69,7 +85,12 @@ handleSubmit = event => {
           <label className="form-label" id="about" htmlFor="about">
             About
           </label>
-          <textarea name="about" value={this.state.about} onChange={this.handleChange} className="form-input"/>
+          <textarea
+            name="about"
+            value={this.state.about}
+            onChange={this.handleChange}
+            className="form-input"
+          />
         </div>
         <button className="submit-button" type="submit">
           Submit

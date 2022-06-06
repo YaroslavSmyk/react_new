6f6424ecac7a943d95e27b87f1323a9d831cddd0
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-06-06 22:07:40 +0300

    v1

---
+++ lesson9/task5/src/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './Game.jsx';
+
+const rootElement = document.querySelector('#root');
+
+
+ReactDOM.render(<Game />, rootElement);

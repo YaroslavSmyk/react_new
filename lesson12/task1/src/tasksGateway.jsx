7cbab04097beeb265a0383d9e317ddce39a69d20
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-06-13 23:30:23 +0300

    v2

--- lesson12/task1/src/tasksGateway.jsx
+++ lesson12/task1/src/tasksGateway.jsx
@@ -2,7 +2,7 @@ const baseUrl = "https://626c06265267c14d566b9473.mockapi.io/api/v1/TodoList";
 
 export const createTask = (taskData) => {
   return fetch(baseUrl, {
-    mothod: "POST",
+    method: "POST",
     headers: {
       "Content-Type": "application/json;utc-8",
     },
@@ -31,7 +31,7 @@ export const fetchTasksList = () => {
 
 export const updateTask = (taskId, taskData) => {
   return fetch(`${baseUrl}/${taskId}`, {
-    mothod: "PUT",
+    method: "PUT",
     headers: {
       "Content-Type": "application/json;utc-8",
     },
@@ -45,7 +45,7 @@ export const updateTask = (taskId, taskData) => {
 
 export const deleteTask = (taskId) => {
   return fetch(`${baseUrl}/${taskId}`, {
-    mothod: "DELETE",
+    method: "DELETE",
   }).then((response) => {
     if (!response.ok) {
       throw new Error("Failed to delete task");
